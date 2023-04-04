7be14c8d3c541931fb70ab61a9f0905809e64a82
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-04 21:06:57 +0300

    api documentation setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'drf_spectacular',
     'core',
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
