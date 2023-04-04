40ed46d368914a2d86fc36b788b3b861e8ae1332
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-04 14:26:06 +0300

    adding sample tests

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+    """Add x and y and return the result"""
+    return x + y
+
+def subs(x, y):
+    """Subtract y from x and return the result"""
+    return x - y
